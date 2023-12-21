d868f98e727c6b2ce71b0039a6a7593c4bf27f13
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2023-12-21 20:45:10 +0600

    task completed section done

--- src/DashboardLayout/MyTask/MyTask.jsx
+++ src/DashboardLayout/MyTask/MyTask.jsx
@@ -16,6 +16,7 @@ const MyTask = () => {
     const { currentUser } = useContext(FrankStoreData)
     const [isPending, todoData, refetch] = useGetTodoData(currentUser?.useremail,'todo')
     const [loadingdata, ongoingData, refetchdata] = useGetTodoData(currentUser?.useremail,'ongoing')
+    const [pending, completedData, fetchagain] = useGetTodoData(currentUser?.useremail,'completed')
     // console.log(ongoingData);
     const { register, handleSubmit, formState: { errors }, reset } = useForm()
     const [loading, setloading] = useState(false)
@@ -120,7 +121,7 @@ const MyTask = () => {
         <div className='px-3'>
             {showtaskform && <>
                 <div onClick={() => setshowtaskform(false)} className='bg-black bg-opacity-10 absolute min-h-screen block w-full top-0 left-0'></div>
-                <form onSubmit={handleSubmit(onSubmit)} className='w-72 md:w-[50%] flex flex-col items-start gap-2 justify-start z-40 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]' >
+                <form onSubmit={handleSubmit(onSubmit)} className='w-72 md:w-[50%] bg-white p-3 flex flex-col items-start gap-2 justify-start z-40 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]' >
                     <IoCloseSharp onClick={() => setshowtaskform(false)} className='text-4xl ml-auto cursor-pointer hover:bg-red-600 transition-all' />
                     <input className="block outline-none border-b-2 w-full mx-auto p-2 pl-0 border-b-gray-400" type="text" placeholder="Task title" {...register("title", { required: true })} />
                     {errors.title && <p className="text-red-500 ">title is required*</p>}
@@ -193,10 +194,9 @@ const MyTask = () => {
                             }
                             {
                                 (openacordian && item._id === showid) && <span className='flex justify-start items-center p-2 gap-2'>
-                                    <FaHourglassStart onClick={() => taskStart(item._id)} className='cursor-pointer hover:text-yellow-500' data-tooltip-id={`start_task${item._id}`} data-tooltip-content="start task" />
                                     <FaCheck onClick={()=>taskcomplete(item._id)} className='cursor-pointer hover:text-green-600' data-tooltip-id={`complete_task${item._id}`} data-tooltip-content="complete task" />
                                     <FaRegEdit className='cursor-pointer hover:text-blue-600' data-tooltip-id={`edit_task${item._id}`} data-tooltip-content="edit task" />
-                                    <MdDelete className='cursor-pointer hover:text-red-600' data-tooltip-id={`delete_task${item._id}`} data-tooltip-content="delete task" /> </span>
+                                   </span>
                             }
                             <Tooltip id={`start_task${item._id}`} />
                             <Tooltip id={`complete_task${item._id}`} />
@@ -217,6 +217,37 @@ const MyTask = () => {
                 <div className='p-2 rounded shadow-2xl bg-yellow-100 w-full'>
                     <h3 className='text-2xl font-bold text-blue-600'>Completed</h3>
                     <hr className='w-[100%] h-[2px] mx-auto bg-blue-500' />
+                    {
+                        completedData.map(item => <div className={`relative px-2 overflow-hidden bg-yellow-50 shadow my-2 rounded`} key={item._id}>
+                            {
+                                (openacordian && item._id === showid) ? <FaMinus onClick={() => {
+                                    setShowid(null)
+                                    setopenacordian(false)
+                                }} className='absolute top-[50%] translate-y-[-50%] right-2 cursor-pointer  hover:text-blue-600' /> : <FaPlus onClick={() => {
+                                    setShowid(item._id)
+                                    setopenacordian(true)
+                                }} className='absolute top-[50%] translate-y-[-50%] right-2 cursor-pointer  hover:text-blue-600' />
+                            }
+                            {
+                                (openacordian && item._id === showid) && <span className='flex justify-start items-center p-2 gap-2'>
+                                  
+                                    <MdDelete className='cursor-pointer hover:text-red-600' data-tooltip-id={`delete_task${item._id}`} data-tooltip-content="delete task" /> </span>
+                            }
+                            <Tooltip id={`start_task${item._id}`} />
+                            <Tooltip id={`complete_task${item._id}`} />
+                            <Tooltip id={`edit_task${item._id}`} />
+                            <Tooltip id={`delete_task${item._id}`} />
+                            <h3 className=' py-1 font-semibold pr-2'>{item?.title}</h3>
+                            <div className={`${(openacordian && item._id === showid) ? 'h-auto' : 'h-[1px]'} overflow-hidden transition-all`}>
+                                <span>
+                                    priority : <span className={`${item?.priority === '3' ? 'text-red-600' : item?.priority === '2' ? 'text-yellow-600' : 'text-green-600'} font-semibold`} >
+                                        {item?.priority === '3' ? 'high' : item?.priority === '2' ? 'moderate' : 'Low'}
+                                    </span></span>
+                                <p>deadline : {item.deadline}</p>
+                                <p>{item.description}</p>
+                            </div>
+                        </div>)
+                    }
                 </div>
             </div>
         </div>
